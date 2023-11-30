4c34103aa82b29b05abee22b306c4f2ff80723da
ArtemSG <artemsgor@gmail.com>
2023-11-30 14:09:49 +0200

    Lesson04

---
+++ Lesson04/src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    int weels;
+    int doors;
+    public void setWeels(int weels) {
+        this.weels = weels;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+}
---
+++ Lesson04/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello";
+//        System.out.println(str.length());
+        String str1 = "Hello world!";
+//        System.out.println(str1.length());
+//
+//        System.out.println(str1.charAt(0));
+//        System.out.println(str1.charAt(str1.length() - 1));
+//        System.out.println(str1.charAt(100));
+//        System.out.println(str1.substring(1));
+//        System.out.println(str1.substring(1, 5));
+//        System.out.println(str1.substring(1, 5) + " " + str1.substring(7));
+
+//        System.out.println(str1 + " -> " + str1.toLowerCase());
+//        System.out.println(str1 + " -> " + str1.toUpperCase());
+
+//        int number1 = 'A';
+//        System.out.println(number1);
+//        int number2 = Integer.valueOf('A');
+//        System.out.println(number2);
+//        int number3 = Character.getNumericValue('A');
+//        System.out.println(number3);
+
+        System.out.print("Input a num: ");
+        System.out.println("Input a num: ");
+
+
+
+
+
+//      Car - class
+//      car1 - variable
+//      new Car() - create object
+//        Car car1 = new Car();
+//        car1.setWeels(4);
+//        car1.setDoors(2);
+//
+//        Car car2 = new Car();
+//        car2.setWeels(6);
+//        car2.setDoors(4);
+    }
+}
