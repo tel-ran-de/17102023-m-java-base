5f21987563fe285be58457101521bcebd7476b80
ArtemSG <artemsgor@gmail.com>
2023-12-15 14:56:14 +0100

    Lesson14 && Lesson16 prsentation

---
+++ Lesson14/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+//        int count = 10;
+
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("i = " + i);
+
+        for (int j = 0; j <= 10; j++) {
+            System.out.println(j);
+        }
+//        System.out.println(j);
+//
+//        while (true) {
+//            System.out.println("Hello");
+//        }
+
+//        Используйте цикл do-while
+//        Инициализируйте переменную int i = 0;
+//        Добавьте в тело инструкцию System.out.println("Я в теле DO");
+//        Определите условие i < 3
+//        Запустите код
+//        Определите условие i < 0
+//        Проанализируйте результат
+
+//        int i = 0;
+//        int k = 0;
+//        do {
+//            System.out.println("Я в теле DO");
+//            i++;
+//            k++;
+//            System.out.println(k);
+//        }
+//        while (i < 3 && k < 3);
+//
+//        int j = 0;
+//        while (j < 0) {
+//            System.out.println("Я в теле DO");
+//            j++;
+//        }
+
+//        int x = 20;
+//        int sum = 0;
+//
+//        do {
+//            sum += x;
+//            x--;
+//        } while(x > 10);
+//        System.out.println(sum);
+    }
+}
